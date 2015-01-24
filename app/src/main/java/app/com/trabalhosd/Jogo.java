38d7d0653eccb2a65d4d0a2fc071c0fb7ee6ad2f
Carlos <fonseca.carlosh@gmail.com>
2015-01-24 19:39:35 -0200

    Implementação da ação do Plantador

--- app/src/main/java/app/com/trabalhosd/Jogo.java
+++ app/src/main/java/app/com/trabalhosd/Jogo.java
@@ -11,22 +11,26 @@ import java.util.ArrayList;
 
 public class Jogo extends Activity {
 
-    public static ArrayList<Planta> jardim = new ArrayList<Planta>();
+    public static ArrayList<Planta> jardim = new ArrayList<Planta>(9);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jogo);
 
-        final ImageButton c11 = (ImageButton) findViewById(R.id.c11);
+        for(int i = 0; i < 9; i++){
+            Planta pt = new Planta();
+            jardim.add(pt);
+        }
+        /*final ImageButton c11 = (ImageButton) findViewById(R.id.c11);
         c11.setBackgroundResource((R.drawable.grass2));
         c11.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               /* status.get(0)++;
-                if(status[0][0] > 3){
-                    status[0][0] = 3;
+                int statusAtual = jardim.get(0).getStatus();
+                if(statusAtual < 3){
+                    jardim.get(0).setStatus(statusAtual++);
                 }
-                switch(status[0][0]){
+                switch(jardim.get(0).getStatus()){
                     case 0:
                         c11.setBackgroundResource(R.drawable.grass2);
                         break;
@@ -39,13 +43,46 @@ public class Jogo extends Activity {
                     case 3:
                         c11.setBackgroundResource(R.drawable.tree2);
                         break;
-                }*/
+                }
 
             }
-        });
+        });*/
 
     }
 
+    public void acao(View view){
+        int botao = 0;
+        switch(view.getId()){
+            case R.id.planta_0:
+                botao = 0;
+                break;
+            case R.id.planta_1:
+                botao = 1;
+                break;
+            case R.id.planta_2:
+                botao = 2;
+                break;
+        }
+
+
+        int statusAtual = jardim.get(botao).getStatus();
+            jardim.get(botao).setStatus(statusAtual+1);
+
+        switch(jardim.get(botao).getStatus()){
+            case 0:
+                view.setBackgroundResource(R.drawable.grass2);
+                break;
+            case 1:
+                view.setBackgroundResource(R.drawable.seed);
+                break;
+            case 2:
+                view.setBackgroundResource(R.drawable.bush);
+                break;
+            case 3:
+                view.setBackgroundResource(R.drawable.tree2);
+                break;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
