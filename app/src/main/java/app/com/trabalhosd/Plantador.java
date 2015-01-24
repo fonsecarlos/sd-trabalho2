39c4764efb1c09f0dc53ca70e35e48fd9573fda4
Carlos <fonseca.carlosh@gmail.com>
2015-01-24 18:25:40 -0200

    Implementação do Lenhador e Plantador

--- app/src/main/java/app/com/trabalhosd/Plantador.java
+++ app/src/main/java/app/com/trabalhosd/Plantador.java
@@ -4,4 +4,30 @@ package app.com.trabalhosd;
  * Created by caik on 24/01/15.
  */
 public class Plantador {
+    private int velocidade = 1;
+
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public void plantar(int planta){
+        statusAtual = Jogo.jardim.get(planta).getStatus();
+        if(statusAtual == 0){
+            Jogo.jardim.get(planta).setStatus(1);
+        }
+    }
+
+    public void regar(int planta){
+        statusAtual = Jogo.jardim.get(planta).getStatus();
+        if(statusAtual != 0 && statusAtual != 3){
+            Jogo.jardim.get(planta).setStatus(statusAtual++);
+        }
+    }
+
+
 }
