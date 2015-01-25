d072005826372d6217d0f94d97df301dd19ef4f3
Patricia Haizer <paty.haizer@gmail.com>
2015-01-25 15:19:53 -0200

    Implementação inicial do back end

--- app/src/main/java/app/com/trabalhosd/MenuPrincipal.java
+++ app/src/main/java/app/com/trabalhosd/MenuPrincipal.java
@@ -3,19 +3,83 @@
 package app.com.trabalhosd;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.ActionListener;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.ChannelListener;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import app.com.trabalhosd.DeviceListFragment.DeviceActionListener;
 
-public class MenuPrincipal extends Activity {
+public class MenuPrincipal extends Activity implements
+        ChannelListener, DeviceActionListener {
+
+    public static final String TAG = "MenuPrincipal";
+
+    private WifiP2pManager mManager;
+
+    private final IntentFilter intentFilter = new IntentFilter();
+    private WifiP2pManager.Channel mChannel;
+    private BroadcastReceiver receiver = null;
+    boolean isWifiP2pEnabled = false;
+
+    /**
+     * @param isWifiP2pEnabled the isWifiP2pEnabled to set
+     */
+    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        //  Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                Toast.makeText(MenuPrincipal.this, "Discovery Initiated",
+                        Toast.LENGTH_SHORT).show();
+                Log.d(MenuPrincipal.TAG, "Discovery Initiated");
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                Toast.makeText(MenuPrincipal.this, "Discovery Failed : " + reasonCode,
+                        Toast.LENGTH_SHORT).show();
+                Log.d(MenuPrincipal.TAG, "Discovery Failed");
+            }
+        });
+
     }
 
 
@@ -29,12 +93,11 @@ public class MenuPrincipal extends Activity {
     public void iniciarJogo(View view){
         Intent telaJogo = new Intent(this, Jogo.class);
         startActivity(telaJogo);
-
-
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -46,5 +109,59 @@ public class MenuPrincipal extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** register the BroadcastReceiver with the intent values to be matched */
+    @Override
+    public void onResume() {
+        super.onResume();
+        receiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    /**
+     * Remove all peers and clear all fields. This is called on
+     * BroadcastReceiver receiving a state change event.
+     */
+    public void resetData() {
+
+    }
+
+
+    @Override
+    public void showDetails(WifiP2pDevice device) {
+
+    }
+
+    @Override
+    public void connect(WifiP2pConfig config) {
+
+    }
+
+    @Override
+    public void disconnect() {
+
+    }
+
+    @Override
+    public void onChannelDisconnected() {
+
+    }
+
+    @Override
+    public void cancelDisconnect() {
+
+        /*
+         * A cancel abort request by user. Disconnect i.e. removeGroup if
+         * already connected. Else, request WifiP2pManager to abort the ongoing
+         * request
+         */
+
     }
 }
